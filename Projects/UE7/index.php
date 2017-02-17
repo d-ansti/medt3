bd8d117f8945e253a76afc233699a4dd656f8bf9
Dominik Arnstorfer <arnstorfer.dominik@gmail.com>
2017-02-17 12:33:55 +0100

    Changed

--- Projects/UE7/index.php
+++ Projects/UE7/index.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Index</title>
+    <title>dbAccess</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -19,6 +19,7 @@
       <br>
 
 <?php
+
 // DB Settings
 $host = 'localhost';
 $dbname = 'medt3';
@@ -27,18 +28,17 @@ $pwd = '';
 
 // Establish & check connection
 try {
-$db = new PDO ( "mysql:host=$host;dbname=$dbname", $user, $pwd);
-
+   $db = new PDO ( "mysql:host=$host;dbname=$dbname", $user, $pwd);
+   echo "<h1>Connected successfully to database $dbname</h1>";
 } catch (PDOException $e) {
-   print "<h1>Error: " . $e->getMessage()."</h1>";
+   echo "<h1>Error: " . $e->getMessage()."</h1>";
    die();
-}
-if ($db) {
-  echo "<h1>Connected successfully to database $dbname</h1>";
 }
 
 // SELECT Statement
 $sql = "SELECT * FROM project";
+$res = $db->query($sql);
+$tmp = $res->fetchAll(PDO::FETCH_OBJ);
 
 ?>
 <hr>
@@ -49,28 +49,27 @@ $sql = "SELECT * FROM project";
       <th>name</th>
       <th>description</th>
       <th>createDate</th>
+      <th>operations</th>
     </tr>
   </thead>
   <tbody>
 
 <?php
 
+
 // Mit Bootstrap
-foreach ($db->query($sql) as $row) {
+foreach ($tmp as $row) {
   echo "<tr>";
-  echo "<td>";
-  echo $row['id'];
-  echo "</td>";
-  echo "<td>";
-  echo $row['name'];
-  echo "</td>";
-  echo "<td>";
-  echo $row['description'];
-  echo "</td>";
-  echo "<td>";
-  echo $row['createDate'];
-  echo "</td>";
+  echo "<td>" . $row->id . "</td>";
+  echo "<td>" . $row->name . "</td>";
+  echo "<td>" . $row->description . "</td>";
+  echo "<td>" . $row->createDate . "</td>";
+  echo "<td>
+    <a href=\"index.php?change=$row->id\"><span class=\"glyphicon glyphicon-pencil\"></span>
+    <a href=\"index.php?delete=$row->id\"><span class=\"glyphicon glyphicon-trash\"></span>
+  </td>";
+
+
   echo "</tr>";
 }
-
 ?>
