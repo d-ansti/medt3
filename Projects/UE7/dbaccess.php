eba885e92f5c13e7e338247dac6b9e7031a44e42
Dominik Arnstorfer <arnstorfer.dominik@gmail.com>
2017-02-22 14:29:53 +0100

    Changed

--- Projects/UE7/dbaccess.php
+++ Projects/UE7/dbaccess.php
@@ -57,7 +57,7 @@ $tmp = $res->fetchAll(PDO::FETCH_OBJ);
 <?php
 
 
-// Mit Bootstrap
+// Show Table
 foreach ($tmp as $row) {
   echo "<tr>";
   echo "<td>" . $row->id . "</td>";
@@ -65,11 +65,62 @@ foreach ($tmp as $row) {
   echo "<td>" . $row->description . "</td>";
   echo "<td>" . $row->createDate . "</td>";
   echo "<td>
-    <a href=\"index.php?change=$row->id\"><span class=\"glyphicon glyphicon-pencil\"></span>
-    <a href=\"index.php?delete=$row->id\"><span class=\"glyphicon glyphicon-trash\"></span>
+    <a href=\"dbaccess.php?change=$row->id\"><span class=\"glyphicon glyphicon-pencil\"></span>
+    <a href=\"dbaccess.php?delete=$row->id\"><span class=\"glyphicon glyphicon-trash\"></span>
   </td>";
 
 
   echo "</tr>";
 }
+
+// INSERT Statement
+echo "<p><a href=\"dbaccess.php?insert\"><button type=\"button\" class=\"btn btn-default\">Insert</button></a>";
+
+if (isset($_GET['insert'])) {
+  $ins = "
+USE medt3;
+DROP TABLE IF EXISTS project;
+CREATE TABLE project (
+	id INTEGER NOT NULL auto_increment,
+	name varchar(255) NOT NULL,
+	description text,
+	createDate DATETIME NOT NULL,
+	PRIMARY KEY (id)
+);
+INSERT INTO project (name, description, createDate) VALUES
+	('Demo App A','Some text','2014-02-10 12:00:00'),
+	('Demo App B','Some text text','2014-02-10 12:01:00'),
+	('Demo App C','Some text text text','2014-02-10 12:02:00'),
+	('Demo App D','Some text text text text','2014-02-07 12:02:00'),
+	('Demo App E','Some text text text text text','2014-02-09 11:02:00'),
+	('Demo App F','Some text','2014-02-10 12:00:00'),
+	('Demo App G','Some text text','2014-02-10 12:01:00'),
+	('Demo App H','Some text text text','2014-02-10 12:02:00'),
+	('Demo App I','Some text text text text','2014-02-07 12:02:00');";
+  $db->exec($ins);
+  header("refresh: 0; url=inserted_all");
+} echo "</p>";
+
+// DELETE Statement
+if (isset($_GET['delete'])) {
+  $del = "DELETE FROM project WHERE id = ".$_GET['delete'];
+  $db->exec($del);
+  header("refresh: 0; url=element_deleted");
+}
+
+// UPDATE Statement
+
+// Show RewriteRule
+/*
+echo $_SERVER['REQUEST_URI'];
+echo "<br> GET:";
+var_dump($_GET);
+echo "<br> POST:";
+var_dump($_POST);
+echo "<br>";
+*/
 ?>
+
+    </div>
+  </body>
+</html>
